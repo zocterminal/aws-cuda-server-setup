f70b2b3ff8a6d8872c8121c4d3f5a059b11299f5
Markus Schmidt <m.schmidt@emtec.com>
2024-04-13 17:39:36 +0200

    instructions step 3

--- src/install_step3.sh
+++ src/install_step3.sh
@@ -20,8 +20,29 @@ mv llm.c llm
 cd llm
 
 
+cat <<EOF
+Check if CUDA install is complete then log out and log in again
 
-echo 'Now view the README at https://github.com/karpathy/llm.c'
-echo 'Check if CUDA install is complete then log out and log in again'
-echo ''
+View the README at https://github.com/karpathy/llm.c
 
+Commands from the README/tutorial to run: 
+- python prepro_tinyshakespeare.py
+- python train_gpt2.py
+
+- make train_gpt2
+- OMP_NUM_THREADS=8 ./train_gpt2
+
+- make test_gpt2
+- ./test_gpt2
+
+- make test_gpt2cu
+- ./test_gpt2cu
+
+- make train_gpt2cu
+- ./train_gpt2cu
+
+- python train_gpt2.py --inference_only 1 --write_tensors 0 --sequence_length 1024 --batch_size 4
+
+- python train_gpt2.py --inference_only 1 --write_tensors 0 --sequence_length 1024 --batch_size 4 --compile 1 --tensorcores 1
+
+EOF
